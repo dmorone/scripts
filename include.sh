5c025a842075ccdedc77485d8abd7b7efc41ca1e
dmorone <diegomorone@protonmail.com>
2025-12-16 13:41:57 +0100

    added aliases, fixed unused commands

---
+++ include.sh
@@ -0,0 +1,23 @@
+alias em='emacs'
+alias emnw='emacsnw'
+
+alias ..='cd ..'
+alias 2..='cd ../..'
+alias 3..='cd ../../..'
+alias 4..='cd ../../../..'
+alias 5..='cd ../../../../..'
+
+mkcd () {
+  \mkdir -p "$1"
+  cd "$1"
+}
+
+tempe () {
+  cd "$(mktemp -d)"
+  chmod -R 0700 .
+  if [[ $# -eq 1 ]]; then
+    \mkdir -p "$1"
+    cd "$1"
+    chmod -R 0700 .
+  fi
+}
