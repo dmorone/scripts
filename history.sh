5ba876f68bd95affcfb0810a9f09b369500cb5c0
dmorone <diegomorone@protonmail.com>
2026-01-10 19:56:30 +0100

    added htpasswd file and trying starship prompt

--- history.sh
+++ history.sh
@@ -1,6 +1,12 @@
+export HISTSIZE=1000000000
+export SAVEHIST=$HISTSIZE
+setopt EXTENDED_HISTORY
+
 # Search past commands
 bindkey "^[[A" history-search-backward
 bindkey "^[[B" history-search-forward
+
 # Enable fuzzy search for history
 # brew install fzf
-export FZF_DEFAULT_COMMAND='history -100000'
+# export FZF_DEFAULT_COMMAND='history -100000'
+source <(fzf --zsh)
