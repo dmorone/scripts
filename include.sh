0e19eb37c6e9c16b4b4a55ba22af5b1e6303c0bc
dmorone <diegomorone@protonmail.com>
2026-01-12 16:19:54 +0100

    ZSH history and starship

--- include.sh
+++ include.sh
@@ -1,3 +1,16 @@
+# ZSH. Install starship
+export STARSHIP_CONFIG=~/.scripts/starship.toml
+eval "$(starship init zsh)"
+
+# History
+export HISTFILE="$HOME/.zsh_history"
+export HISTSIZE=1000000000
+export SAVEHIST=1000000000
+setopt EXTENDED_HISTORY
+
+eval "$(fzf --zsh)" # fuzzy search. Install fzf. Use Ctrl+R
+
+# Stuff that I use
 alias em='emacs'
 alias emnw='emacsnw'
 
