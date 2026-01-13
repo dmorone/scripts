7367682fd1f8f806df8c9f9c8a7178dd8ce5942d
dmorone <diegomorone@protonmail.com>
2026-01-13 16:28:14 +0100

    shell improvements

--- include.sh
+++ include.sh
@@ -1,6 +1,9 @@
 # ZSH. Install starship
 export STARSHIP_CONFIG=~/.scripts/starship.toml
-eval "$(starship init zsh)"
+eval "$(starship init zsh)" # Install with brew
+# remember to install a font with icons
+# (e.g., Fira code https://github.com/tonsky/FiraCode)
+# and set it in terminal app
 
 # History
 export HISTFILE="$HOME/.zsh_history"
@@ -8,17 +11,18 @@ export HISTSIZE=1000000000
 export SAVEHIST=1000000000
 setopt EXTENDED_HISTORY
 
-eval "$(fzf --zsh)" # fuzzy search. Install fzf. Use Ctrl+R
+eval "$(fzf --zsh)" # fuzzy search. Install fzf with brew. Use Ctrl+R
 
 # Stuff that I use
 alias em='emacs'
-alias emnw='emacsnw'
+alias emnw='emacs --no-window-system'
 
 alias ..='cd ..'
 alias 2..='cd ../..'
 alias 3..='cd ../../..'
 alias 4..='cd ../../../..'
 alias 5..='cd ../../../../..'
+setopt autocd
 
 mkcd () {
   \mkdir -p "$1"
